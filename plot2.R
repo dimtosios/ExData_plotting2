a7f3966b64a8ca325e92c7a642e94b579b364508
dimtosios <dimtosios@gmail.com>
2016-12-11 22:35:44 +0200

    Add files via upload

--- plot2.R
+++ plot2.R
@@ -3,10 +3,9 @@ setwd("C:/Users/dimitris/Desktop/R programming/exdata/project2")
 NEI <- readRDS("summarySCC_PM25.rds")
 baltimore <- subset(NEI, fips == "24510")
 aggdata<-aggregate(Emissions~year,baltimore,sum)
+
 png('plot2.png')
 
-## plot data
-plot(baltimore$year, baltimore$Emissions, type = "l", 
-     main = "Total PM2.5 Emission in the Baltimore 1999-2008",
-     xlab = "Year", ylab = "Emissions")
+barplot(height=aggdata$Emissions, names.arg=aggdata$year, xlab="years", ylab=expression('total PM'[2.5]*' emission'),main=expression('Total PM'[2.5]*' in the Baltimore City emissions at various years'))
+
 dev.off()
